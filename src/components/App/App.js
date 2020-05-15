a6fa531867589d3a1300e749064e2668b8070ecf
Alan Henderson <mr.alan.henderson@gmail.com>
2020-05-15 17:00:06 -0500

    added a linking database, initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import ()
 
 class App extends Component {
+
+//component did mount will need to send "FETCH" messages
+
   // Renders the entire app on the DOM
   render() {
     return (
