b4da00088aa6cda2b2bdc3b7bf83c7cb7ae70f51
Alan Henderson <mr.alan.henderson@gmail.com>
2020-05-15 18:37:54 -0500

    more set up done, everythign still good

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import ()
+import {connect} from 'react-redux';
 
 class App extends Component {
 
 //component did mount will need to send "FETCH" messages
+componentDidMount(){
+  console.log("didMount", this.props);
+  // this.props.dispatch({ type: 'SET_MOVIES'})
+  // this.props.dispatch({ type: 'SET_GENRES'})
+}
 
+  clickHandler = () => {
+    console.log('in clickHandler');
+    this.props.dispatch({ type: 'click' });
+  }
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
         <p>Empty Page</p>
+        <button onClick={this.clickHandler}>Click Me</button>
       </div>
     );
   }
 }
 
-export default App;
+
+const mapReduxStateToProps = (reduxState) => ({ reduxState });
+
+export default connect(mapReduxStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import logger from 'redux-logger';
 // Import saga middleware
 import createSagaMiddleware from 'redux-saga';
 
+
 // Create the rootSaga generator function
 function* rootSaga() {
 
